b0d57131b42658f1ca21c8a17dba274e26fb389e
Steven Branson <steve@diodeware.com>
2019-02-14 09:43:26 -0600

    Create Hashtag and Tweet entities

---
+++ DotnetAssessmentSocialMedia/Data/Entities/Hashtag.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DotnetAssessmentSocialMedia.Data.Entities
+{
+    [Table("hashtag")]
+    public class Hashtag
+    {
+        [Required]
+        [Key]
+        [Column("label")]
+        public string Label {
+            get {
+                return Label;
+            }
+
+            set {
+                value.ToLower();
+            }
+        }
+
+        [Required]
+        [Column("first_used")]
+        public DateTime FirstUsed { get; set; }
+
+        [Required]
+        [Column("last_used")]
+        public DateTime LastUsed { get; set; }
+    }
+}
---
+++ DotnetAssessmentSocialMedia/Data/Entities/Tweet.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DotnetAssessmentSocialMedia.Data.Entities
+{
+    [Table("tweet")]
+    public class Tweet
+    {
+        [Key]
+        [Column("id")]
+        public int Id { get; set; }
+
+        [Required]
+        [Column("author")]
+        public User Author { get; set; }
+
+        [Required]
+        [Column("posted")]
+        public DateTime Posted { get; set; }
+        
+        [Column("content")]
+        public string Content { get; set; }
+
+        [ForeignKey("tweet")]
+        [Column("replying_to_id")]
+        public int? InReplyTo { get; set; }
+
+        [ForeignKey("tweet")]
+        [Column("repost_of_id")]
+        public int? RepostOf { get; set; }
+    }
+}
